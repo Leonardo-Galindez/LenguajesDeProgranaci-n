c8b3923ce7a87ba0f42569b8f22401ab8d0a9180
Leonardo-Galindez <leonardogalindez2018@gmail.com>
2024-03-20 22:30:57 -0300

    ejercicio 4 terminado

---
+++ TP-N°2/ejercicio4.pas
@@ -0,0 +1,25 @@
+Program ejercicio4;
+Var num : real;
+function mayor(numero : Real): Boolean;
+begin
+    mayor := numero > 0;
+end;
+Begin
+    write('Ingrese numero');
+    read(num);
+    if mayor(num) then
+    begin
+        writeln('El numero es mayor que "0"');
+    end
+    else
+    begin
+        if num < 0 then
+        begin
+            writeln('El numero es menor que "0"');
+        end
+        else
+        begin
+            writeln('El numero es igual a "0"');
+        end;
+    end;
+End.
