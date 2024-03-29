79f9c3b80de3e66f9c434af527c650ae245847ba
Leonardo-Galindez <leonardogalindez2018@gmail.com>
2024-03-29 14:31:24 -0300

    ejercicio7 terminado

---
+++ TP-N°2/ejercicio7.pas
@@ -0,0 +1,10 @@
+PROGRAM ejercicio7;
+VAR num, i : integer;
+BEGIN
+    writeln('Ingrese numero:');
+    read(num);
+    FOR i := num to num + 19 DO
+        Begin
+            write(i);
+        End;
+END.
