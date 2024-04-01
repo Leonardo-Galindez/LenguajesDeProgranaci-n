732fdbbabc9cfc34af10a3071e67d1cface1d7b5
Leonardo-Galindez <leonardogalindez2018@gmail.com>
2024-04-01 00:09:37 -0300

    c

--- miPrimerPrograma.pas
+++
@@ -1,9 +0,0 @@
-PROGRAM ejemplo; { Cabecera } 
-CONST max=100; { Constantes } 
-VAR a:integer; { Variables } 
-BEGIN { <== inicia el programa principal } 
-write ('Escriba␣un␣número:␣'); 
-read(a); 
-a := a+max; 
-writeln('El␣resultado␣de␣A␣+␣100␣es:␣',a); 
-END. { <== finaliza el programa principal }
