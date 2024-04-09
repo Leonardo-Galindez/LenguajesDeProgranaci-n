c7dc1a8421deffc8cba657a838b2321bbb34206e
Leonardo-Galindez <leonardogalindez2018@gmail.com>
2024-04-09 09:47:50 -0300

    cambios

--- TP-N°2/ejercicio23.pas
+++ TP-N°2/ejercicio23.pas
@@ -2,12 +2,13 @@ PROGRAM ejercicio23;
 uses
   SysUtils; // Incluye la unidad SysUtils para usar StrToIntDef
 VAR cadena: String ;
-VAR i , j , e , u: integer;
+VAR i , j , e , u , code: integer;
 VAR num1S , num2S : String;
+VAR num1 , num2 : integer;
 VAR subStrings : array[1 .. 3] of String;
-VAR resultado : real;
+VAR resultado : integer;
 
-function realizarOperacion(simbolo : String; num1 : integer ; num2 : integer):real;
+function realizarOperacion(simbolo : String; num1 : integer ; num2 : integer):integer;
 begin
     if simbolo = 'mas' then 
     begin
@@ -24,16 +25,17 @@ end;
 procedure obtenerCadenas(cadena : String);
 begin
     j := 0;
-    e := 0;
+    e := 1;
     u := 0;
     FOR i := 0 TO length(cadena) DO
     begin
-        if cadena[u]<>' ' then
+        if cadena[u] = ' ' then
         begin
             subStrings[e] := Copy(cadena,j,u-1);
             e := e + 1;
+            j := u + 1;
+            
         end;
-        j := j + 1;
         u := u + 1;
     end;
 end;
@@ -44,4 +46,7 @@ BEGIN
     obtenerCadenas(cadena);
     num1S := subStrings[1];
     num2S := subStrings[3];
+    Val(num1S,num1,code);
+    Val(num2S,num2,code);
+    writeln('resultado:',realizarOperacion(subStrings[2],num1,num2));
 END.
